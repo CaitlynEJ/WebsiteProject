f5fa2dafda53a92acf9747ab63188877cb7970d0
Caitlyn Jameson <59493115+CaitlynEJ@users.noreply.github.com>
2021-01-15 15:08:48 -0600

    Added files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 9086);
+
+app.use(express.static('public'))
+
+app.get('/library', function(req, res){
+    res.render('library', library);
+});
